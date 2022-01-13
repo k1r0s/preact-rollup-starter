d8b39c508e464b94bf90d0dabf4640379d40d3dd
k1r0s <ciroreed@gmail.com>
2022-01-13 17:28:02 +0100

    test

--- src/components/dummy-component.jsx
+++ src/components/dummy-component.jsx
@@ -5,7 +5,7 @@ import scoped from "./dummy-component.css";
 export const DummyComponent = style(scoped, () => {
   return (
     <article>
-      <h1>Hi from DummyComponent.</h1>
+      <h2>Hi from DummyComponent.</h2>
       <em>Now let's play with Preact!</em>
     </article>
   )
